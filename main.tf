9fe3d3cac6c51a9f4ffd2b55462a016bd0bee902
Vincent Genest <58990690+deltxprt@users.noreply.github.com>
2022-09-13 21:20:49 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,110 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.4.1"
+    }
+  }
+}
+
+variable "VAULTTOKEN" {
+    type = string
+    description = "Vault token for authentication to fetch secrets"
+}
+
+provider "vault" {
+  address         = "https://vault.lab.markplay.net"
+  token           = var.VAULTTOKEN
+}
+
+data "vault_generic_secret" "azsecrets" {
+  path = "Azure/Demo-Terraform"
+}
+
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = data.vault_generic_secret.azsecrets.subid
+  client_id       = data.vault_generic_secret.azsecrets.clientid
+  client_secret   = data.vault_generic_secret.azsecrets.clientsecret
+  tenant_id       = data.vault_generic_secret.azsecrets.tenantid
+}
+
+variable "prefix" {
+  default = "terra"
+}
+
+resource "azurerm_resource_group" "demo-rs" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "demo-vnet" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "demo-subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "demo-nic" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "demo-terraform"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "demo-vm" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v3"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition-smalldisk"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "osdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+  os_profile {
+    computer_name  = "demo-terraform"
+    admin_username = "myadminuser"
+    admin_password = "Password1234!"
+  }
+  os_profile_windows_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "demo"
+    auto        = "terraform"
+  }
+}
